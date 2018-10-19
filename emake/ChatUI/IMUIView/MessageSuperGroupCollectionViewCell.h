cb0c9b2249b3dc304ee1df601a1a41fadb88cac2
demo_zhang <demo_zhang@emake.cn>
2018-10-19 14:05:18 +0800

    no message

---
+++ emake/ChatUI/IMUIView/MessageSuperGroupCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageSuperGroupCollectionViewCell.h
+//  emake
+//
+//  Created by zhangshichao on 2018/10/19.
+//  Copyright © 2018年 emake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageSuperGroupCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ emake/ChatUI/IMUIView/MessageSuperGroupCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MessageSuperGroupCollectionViewCell.m
+//  emake
+//
+//  Created by zhangshichao on 2018/10/19.
+//  Copyright © 2018年 emake. All rights reserved.
+//
+
+#import "MessageSuperGroupCollectionViewCell.h"
+
+@implementation MessageSuperGroupCollectionViewCell
+
+@end
